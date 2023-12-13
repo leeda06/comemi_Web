78e9a65aeb9c8491df481d5e86bf480d414d07c6
leeda06 <88700168+leeda06@users.noreply.github.com>
2023-12-14 01:39:57 +0900

    [add] : Add index file and basic settings

--- Web/Js/header.js
+++ Web/Js/header.js
@@ -27,19 +27,27 @@ function changeBright() {
         document.head.appendChild(link1);
     }
 }
+
+
+// 현재 로드된 HTML 파일의 이름 가져오기
+var currentHtmlFileName = document.location.pathname.split('/').pop().replace(/\.[^/.]+$/, '');
+
+var nameLink = currentHtmlFileName + '.css';
+
 var link1 = document.createElement('link');
 link1.rel = 'stylesheet';
-link1.href = 'Css/calender.css';
+link1.href = 'Css/' + currentHtmlFileName + 's/' + nameLink;
 var link2 = document.createElement('link');
 link2.rel = 'stylesheet';
-link2.href = 'Css/dark-calender.css';
+link2.href = 'Css/' + currentHtmlFileName + 's/dark-' + nameLink;
+
 
 function changeBright() {
     var iconElement = document.getElementById('icon');
     var links = document.getElementsByTagName('link');
 
     for (var i = 0; i < links.length; i++) {
-        if (links[i].href.indexOf('calender.css') !== -1) {
+        if (links[i].href.indexOf(nameLink) !== -1) {
             document.head.removeChild(links[i]);
             break;
         }
